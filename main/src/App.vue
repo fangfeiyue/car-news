30628c55df395decdfc6ab7ced95e568874ce1dd
FangFeiyue <18766131540@163.com>
2022-06-30 18:04:41 +0800

    界面布局

---
+++ main/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <main-header />
+  <bread-crumbs />
+  <micro-body />
+  <global-footer />
+</template>
+<script setup>
+import MainHeader from '@/components/Header'
+import GlobalFooter from '@/components/Footer/index.vue'
+import MicroBody from '@/view/micro-body.vue'
+import BreadCrumbs from '@/components/BreadCrumbs'
+</script>
+<style>
+html, body, #micro_web_main_app {
+  width: 100%;
+  height: 100%;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
